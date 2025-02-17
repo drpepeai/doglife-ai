d8883e30fdfc0fc076f17c94019a43a153653476
Victor Maragioglio <maragiogliov@protonmail.com>
2025-02-17 23:38:00 +0100

    go

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,6 +1,6 @@
 import React, { useRef, useEffect, useState } from "react";
 import { Link } from "react-router-dom"; // Import Link
-import drpepetitle from "../assets/Ai-agent-by-drp.svg"
+
 
 import "./hero.css";
 import doglifeai_title from "../assets/Doglife_funky_title.svg";
@@ -9,7 +9,6 @@ import doggyhead from "../assets/doggy_head_gold_centered.gif"
 
 const Hero = () => {
   const [dropdownOpen, setDropdownOpen] = useState(false);
-  const gridRef = useRef(null);
   const dropdownRef = useRef(null);
   const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
 
@@ -39,45 +38,6 @@ const Hero = () => {
     };
   }, []);
 
-  useEffect(() => {
-
-    const gridItems = gridRef.current.querySelectorAll(".grid-item");
-
-    // Elastic grid animation
-    const handleMouseMove = (e) => {
-      const { clientX, clientY } = e;
-
-      gridItems.forEach((item) => {
-        const rect = item.getBoundingClientRect();
-        const distanceX = clientX - (rect.left + rect.width / 2);
-        const distanceY = clientY - (rect.top + rect.height / 2);
-        const distance = Math.hypot(distanceX, distanceY);
-
-        const maxDistance = 200;
-        const intensity = 1 - Math.min(distance / maxDistance, 1);
-
-        const translateX = -intensity * (distanceX / distance) * 20;
-        const translateY = -intensity * (distanceY / distance) * 20;
-
-        item.style.transform = `translate(${translateX}px, ${translateY}px)`;
-      });
-    };
-
-    const handleMouseLeave = () => {
-      gridItems.forEach((item) => {
-        item.style.transform = "translate(0, 0)";
-      });
-    };
-
-    document.addEventListener("mousemove", handleMouseMove);
-    document.addEventListener("mouseleave", handleMouseLeave);
-
-    return () => {
-      document.removeEventListener("mousemove", handleMouseMove);
-      document.removeEventListener("mouseleave", handleMouseLeave);
-
-    };
-  }, []);
 
 
 
@@ -86,75 +46,62 @@ const Hero = () => {
   return (
     <div className="container">
 
-      {/* Grid */}
-      <div ref={gridRef} className="elastic-grid">
-        {[...Array(64)].map((_, index) => (
-          <div key={index} className="grid-item" />
-        ))}
-      </div>
 
 
-      <div className="container-top-desktop">
-
-  
-            <div className="desktop-dropdown">
               <div className="logo-wired" ref={dropdownRef}>
-                <img src={doggylogo} alt="drpepe logo" width={70} onClick={toggleDropdown} className="doggylogo" />
+                <img src={doggylogo} alt="drpepe logo" width={50} onClick={toggleDropdown} className="doggylogo" />
                 {dropdownOpen && (
                   <div className="dropdown-menu">
-                    <div onClick={() => window.open('tg://resolve?domain=drpepeaiOFFICIAL', '_blank')} className="dropdown-item">Join The Community.</div>
+                    <div onClick={() => window.open('tg://resolve?domain=drpepeaiOFFICIAL', '_blank')} className="dropdown-item">Join The Community</div>
                     <div onClick={() => window.open('https://x.com/DogLifeAI', '_blank')} className="dropdown-item">X @doglifeai</div>
                     <div onClick={() => window.open('https://drpepe.typeform.com/EternalsProgram', '_blank')} className="dropdown-item">Become an Ambassador</div>
                     <div onClick={() => window.open('https://docs.drpepe.ai/', '_blank')} className="dropdown-item">Docs</div>
                   </div>
                 )}
               </div>
-            </div>
 
-            <div className="capsule-image-container">
-              <img src={doggyhead} alt="doggyhead"/>
-            </div>
 
-            <div className="landing-text-container">
+ 
 
-              <div className="overlay-text-title">
-                <img src={doglifeai_title} alt="DOG LIFE AI" width={450} />
-              </div>
+  
 
-              <div className="overlay-text-subtitle-one">
-                <div>
-                  Help dogs live forever
-                </div>
-                <div>
-                 ( because humans are weird ) 
-                </div>
           
+
+
+  
+
+                  <img src={doggyhead} alt="doggyhead" width={260}/>
+      
+
+                  <img src={doglifeai_title} alt="DOG LIFE AI" width={200} />
+
+                  <div  className="text-line">
+                    Help dogs live forever
+                  </div>
+                  <div  className="text-line">
+                    ( because humans are weird ) 
+                  </div>
+                  <div  className="text-line">
+                    an Ai agent by DrPepe.ai
+                  </div>
+          
+                   <div onClick={openDogAgent} className="text-line"> dog agent v.0 1</div>
              
-                <div className="desktop-third-title">
-                <span><img src={drpepetitle} alt="drpepe.ai title" width={300}/></span>
-                </div>
-              </div>
+                   <Link to="/transfer-widget" className="text-line">
+                      <span className="">
+                      buy with $bryan
+                      </span>  
+                   </Link>
 
-              <div className="agents-container">
-                <div className="square-arrow-text-container">
 
-                  <div onClick={openDogAgent} className="button-85"> DOG AGENT v.0 1</div>
+   
+               
 
-                </div>
+            
 
-                <Link to="/transfer-widget" className="square-arrow-text-container">
-                    <span className="buy-bryan-link">
-                  
-                 BUY WITH $BRYAN
+       
 
-                    </span>
-                      
-                </Link>
-              </div>
-
-            </div>
-
-      </div>
+   
 
    
 
